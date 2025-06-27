775a0843679dbf045e77bb634c3492b406fe5a45
vidapogosoft <vidapogosoft@hotmail.com>
2025-06-26 20:31:38 -0500

    camara ionic

--- ionicsqlite1/sliteapp1/src/app/tab1/tab1.page.ts
+++ ionicsqlite1/sliteapp1/src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import {Camera, CameraResultType, CameraSource} from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -8,6 +11,48 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  capturedImage: string | undefined;
+
+
   constructor() {}
+
+  async takePicture()
+  {
+      try {
+        
+        const image = await Camera.getPhoto({
+            quality: 90,
+            allowEditing: false,
+            resultType: CameraResultType.Uri,
+            source: CameraSource.Camera,
+            //saveToGallery: true
+        });
+
+        this.capturedImage = image.webPath
+
+        if(image.base64String)
+        {
+
+        }else if(image.webPath)
+        {
+          const response = await fetch(image.webPath);
+          const blob = await response.blob();
+
+          const reader = new FileReader();
+          reader.readAsDataURL(blob);
+
+          reader.onloadend = () => {
+
+              const base64data = reader.result as string;
+              console.log("Guardar en sqlite");
+
+          }
+
+        }
+
+      } catch (error) {
+        
+      }
+  }
 
 }
