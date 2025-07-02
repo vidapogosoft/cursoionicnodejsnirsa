f967ff7ed1932675156eddab494f4ef2ce277818
vidapogosoft <vidapogosoft@hotmail.com>
2025-07-02 15:34:16 -0500

    ejemplo 2 para clase

---
+++ ionicrest/rickandmorty/src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'intro',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./page/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'intro',
+    loadComponent: () => import('./page/intro/intro.page').then( m => m.IntroPage)
+  },
+  {
+    path: 'character-detail',
+    loadComponent: () => import('./page/character-detail/character-detail.page').then( m => m.CharacterDetailPage)
+  },
+];
